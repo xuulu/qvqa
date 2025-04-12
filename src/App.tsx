1bed0999fc9d2e1c9a83f9f9882a7e1902c2b34f
Administrator <xuulu04@gmail.com>
2025-04-12 18:49:27 +0800

    第二次提交(2025.4.12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-// import { useState } from 'react'
+import {useState, useEffect} from 'react'
 
 import styles from './App.module.scss'
 
+
 function App() {
+    const [now, setNow] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setNow(new Date());
+        }, 1000);
+
+        // 清除定时器
+        return () => clearInterval(timer);
+    }, []);
+
+    // 获取完整的日期和时间字符串
+    const formattedDateTime = now.toLocaleString('zh-CN', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false, // 使用24小时制
+    });
 
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <br/><br/><br/><br/>
                 <h1 className={styles.gradient}> 简 心 运 维 </h1>
+                <br/>
+                <p>{formattedDateTime}</p>
             </div>
             <div className={styles.footer}>
                 &copy;{new Date().getFullYear()} Created by 简心运维
@@ -16,10 +39,9 @@ function App() {
                 <a href="https://beian.miit.gov.cn/" target="_blank">鲁ICP备2023042170号-1</a>
                 &nbsp;&nbsp;
                 <img src='https://img.alicdn.com/tfs/TB1..50QpXXXXX7XpXXXXXXXXXX-40-40.png'
-                     style={{width: '17px', height: 'auto'}} alt="" />
+                     style={{width: '17px', height: 'auto'}} alt=""/>
                 <a href="https://beian.mps.gov.cn/#/query/webSearch?code=37092102000264" rel="noreferrer"
                    target="_blank">鲁公网安备37092102000264</a>
-
             </div>
         </div>
     )
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,64 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import viteImagemin from 'vite-plugin-imagemin' // 图片压缩
 
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false
+            },
+            optipng: {
+                optimizationLevel: 7
+            },
+            mozjpeg: {
+                quality: 30 // 增加质量以提高图片质量
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox'
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false
+                    },
+                    {
+                        name: 'removeComments' // 移除 SVG 文件中的注释
+                    },
+                    {
+                        name: 'removeUselessDefs' // 移除无用的 <defs>
+                    },
+                    {
+                        name: 'cleanupIDs' // 清理未使用的 ID
+                    }
+                ]
+            }
+        })
+    ],
+
+    build: {
+        target: 'esnext',
+        terserOptions: {
+            compress: {
+                //生产环境时移除console
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].[hash].js`,
+                chunkFileNames: `assets/[name].[hash].js`,
+                assetFileNames: `assets/[name].[hash].[ext]`,
+            },
+        },
+    },
+
+
 })

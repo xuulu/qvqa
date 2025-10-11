86abba830d4d9c03309016ce58906edb5665ac3c
xuulu <xuulu04@gmail.com>
2025-10-11 21:17:49 +0800

    重写代码

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',   // 静态导出
+
+    productionBrowserSourceMaps: false,  // 关闭生产环境sourceMap
+
+    experimental: {
+        optimizePackageImports: [
+            'antd',
+            '@ant-design/icons',
+        ]
+    },
+
+
+
+
 };
 
 export default nextConfig;
